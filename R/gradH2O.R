5c9c20f85f8bbc14265b7fe3ca1e3901b3b75972
jedick <j3ffdick@gmail.com>
2019-10-06 10:41:12 +0800

    Update gradH2O1(): add amino acid plots and rQEC

--- R/gradH2O.R
+++ R/gradH2O.R
@@ -8,50 +8,114 @@
 #library(canprot)
 #library(CHNOSZ)
 
-# basis species comparison, from canprot/vignettes/basis_comparison.Rmd 20190713
+# basis species comparison 20190713 / relative water content 20191005
 gradH2O1 <- function(pdf = FALSE) {
-  if(pdf) pdf("gradH2O1.pdf", width = 10, height = 10)
-  # define labels used in the plot
+
+  # check if E. coli data is available
+  if(!exists("ecoli")) stop("please load amino acid compositions of E. coli proteins with data(ecoli)")
+
+  # set up figure
+  if(pdf) pdf("gradH2O1.pdf", width = 6, height = 6)
+  par(mfrow = c(3, 3))
+  par(mar = c(3.2, 3.2, 2.5, 1))
+  par(mgp = c(2.2, 0.7, 0))
+  par(las = 1)
+
+  # define axis labels
   nH2Olab <- expression(italic(n)[H[2] * O])
   nO2lab <- expression(italic(n)[O[2]])
   ZClab <- expression(italic(Z)[C])
-  QEClab <- CHNOSZ::syslab(c("glutamine", "glutamic acid", "cysteine", "H2O", "O2"))
-  CHNOSlab <- CHNOSZ::syslab(c("CO2", "NH3", "H2S", "H2O", "O2"))
-  # get amino acid composition of human proteins (UniProt)
-  aa <- get("human_base", canprot)
-  protein.formula <- CHNOSZ::protein.formula(aa)
-  ZC <- CHNOSZ::ZC(protein.formula)
-  # set up figure
-  par(mfrow = c(2, 2))
-  par(mar = c(3.2, 3.2, 2.5, 1))
-  par(cex = 1.7)
-  par(mgp = c(2.2, 0.7, 0))
-  par(las = 1)
-  # plot the per-residue compositions of the proteins projected into different sets of basis species
-  for(basis in c("QEC", "CHNOS")) {
-    CHNOSZ::basis(basis)
-    protein.basis <- CHNOSZ::protein.basis(aa)
-    protein.length <- CHNOSZ::protein.length(aa)
-    residue.basis <- protein.basis / protein.length
-    # nO2 vs ZC
-    smoothScatter(ZC, residue.basis[, "O2"], xlab = ZClab, ylab = nO2lab, colramp = colorRampPalette(c("transparent", blues9)))
-    if(basis=="QEC") figlab <- "A" else figlab <- "C"
-    label.figure(figlab, yfrac = 0.88, cex = 1.3)
-    # nH2O vs ZC
-    smoothScatter(ZC, residue.basis[, "H2O"], xlab = ZClab, ylab = nH2Olab, colramp = colorRampPalette(c("transparent", blues9)))
-    if(basis=="QEC") figlab <- "B" else figlab <- "D"
-    label.figure(figlab, yfrac = 0.88, cex = 1.3)
-    # add titles
-    if(basis=="QEC") mtext(QEClab, outer = TRUE, cex = 1.7, line = -1.2)
-    if(basis=="CHNOS") mtext(CHNOSlab, outer = TRUE, cex = 1.7, line = -15.7)
-    # add linear fit for QEC basis 20190713
-    if(basis=="QEC") {
-      lm.QEC <- lm(residue.basis[, "H2O"] ~ ZC)
-      nH2O.pred <- predict.lm(lm.QEC, data.frame(ZC = c(-1, 1)))
-      lines(c(-1, 1), nH2O.pred, lty = 2, lwd = 3, col = "grey40")
+
+  # function to plot linear model
+  lmfun <- function(ZC, nH2O, legend.x = NULL, ...) {
+    mylm <- lm(nH2O ~ ZC)
+    lines(c(-1, 1), predict(mylm, data.frame(ZC = c(-1, 1))), ...)
+    # add R-squared text
+    if(!is.null(legend.x)) {
+      R2 <- round(summary(mylm)$r.squared, 2)
+      R2txt <- substitute(italic(R)^2 == R2, list(R2 = R2))
+      legend(legend.x, legend = R2txt, bty = "n")
     }
+    invisible(round(residuals(mylm), 3))
   }
+
+  # function to add plot title and panel label
+  mainlab <- function(main, lab) {
+    title(main, font.main = 1, cex.main = 1)
+    label.figure(lab, xfrac = 0.15, yfrac = 0.92, cex = 1.6)
+  }
+
+  # function to plot nH2O or nO2 vs ZC of amino acids
+  aaplot <- function(ZC, y, ylab, legend.x, main, lab) {
+    plot(ZC, y, type = "p", pch = aminoacids(1), xlab = ZClab, ylab = ylab)
+    mainlab(main, lab)
+    lmfun(ZC, y, legend.x)
+  }
+
+  scatterfun <- function(ZC, nH2O, main, lab) {
+    smoothScatter(ZC, nH2O, xlab = ZClab, ylab = nH2Olab, colramp = colorRampPalette(c("transparent", blues9)))
+    lmfun(ZC, nH2O, lty = 2, lwd = 2, col = "grey40")
+    mainlab(main, lab)
+  }
+
+  # get names of amino acids
+  aa <- aminoacids("")
+  # calculate ZC of the amino acids
+  ZC.aa <- ZC(info(aa, "aq"))
+
+  # get amino acid composition of human proteins (UniProt)
+  human <- get("human_base", canprot)
+  pf.human <- protein.formula(human)
+  ZC.human <- ZC(pf.human)
+
+  # get amino acid compositions of E. coli proteins (UniProt)
+  pf.ecoli <- protein.formula(ecoli)
+  ZC.ecoli <- ZC(pf.ecoli)
+
+  # calculate nH2O and nO2 of amino acids with CHNOS basis
+  basis("CHNOS")
+  species(aa)
+
+  # plot 1: nH2O-ZC of amino acids (CHNOS)
+  aaplot(ZC.aa, species()$H2O, nH2Olab, "bottomleft", "Amino acids (CHNOS)", "A")
+
+  # plot 2: nO2-ZC of amino acids (CHNOS)
+  aaplot(ZC.aa, species()$O2, nO2lab, "bottomright", "Amino acids (CHNOS)", "B")
+
+  # calculate nH2O and nO2 with QEC basis
+  basis("QEC")
+  species(aa)
+
+  # plot 3: nO2-ZC of amino acids (QEC)
+  aaplot(ZC.aa, species()$O2, nO2lab, "bottomright", "Amino acids (QEC)", "C")
+
+  # plot 4: nH2O-ZC of amino acids (QEC)
+  # save the residuals here
+  rQEC <- aaplot(ZC.aa, species()$H2O, nH2Olab, "bottomright", "Amino acids (QEC)", "D")
+  names(rQEC) <- aminoacids(3)
+
+  # plot 5: nH2O-ZC of human proteins (QEC)
+  nH2O.human <- H2OAA(human, basis = "QEC")
+  scatterfun(ZC.human, nH2O.human, "Human proteins (QEC)", "E")
+
+  # plot 6: nH2O-ZC of E. coli proteins (QEC)
+  nH2O.ecoli <- H2OAA(ecoli, basis = "QEC")
+  scatterfun(ZC.ecoli, nH2O.ecoli, "E. coli proteins (QEC)", "F")
+
+  # plot 7: nH2O-ZC of amino acids (rQEC)
+  aaplot(ZC.aa, rQEC, nH2Olab, NULL, "Amino acids (rQEC)", "G")
+
+  # plot 8: nH2O-ZC of human proteins (rQEC)
+  nH2O.human <- H2OAA(human, basis = "rQEC")
+  scatterfun(ZC.human, nH2O.human, "Human proteins (rQEC)", "H")
+
+  # plot 9: nH2O-ZC of E. coli proteins (rQEC)
+  nH2O.ecoli <- H2OAA(ecoli, basis = "rQEC")
+  scatterfun(ZC.ecoli, nH2O.ecoli, "E. coli proteins (rQEC)", "I")
+
   if(pdf) invisible(dev.off())
+  ## output value of rQEC for checking code of H2OAA()
+  #rQEC
 }
 
 # ZC for selected redox gradients 20190715
