87cb9e981b9e8c96cb80c32e8045c2bc1a5ab127
jedick <j3ffdick@gmail.com>
2020-04-25 23:29:31 +0800

    Italicize species names in pdat_saltygenes()

--- R/pdat_saltygenes.R
+++ R/pdat_saltygenes.R
@@ -44,7 +44,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # HMO+10_prot-cytosol, HMO+10_prot-membrane, HMO+10_transcriptomics
     dat <- read.csv(paste0(datadir, "HMO+10.csv.xz"), as.is=TRUE)
     #description <- paste("Bacillus subtilis", stage)
-    description <- "Bacillus subtilis in 6% w/v NaCl vs control"
+    description <- "_Bacillus subtilis_ in 6% w/v NaCl vs control"
     # use selected dataset
     dat <- dat[dat$Experiment==stage, ]
     # count up- or down-expression at each time point
@@ -61,14 +61,14 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
   } else if(study=="WJ02") {
     # 20200419 E. coli, Weber and Jung, 2002
     dat <- read.csv(paste0(datadir, "WJ02.csv.xz"), as.is=TRUE)
-    description <- "E. coli in 0.4 M NaCl vs control"
+    description <- "_Escherichia coli_ in 0.4 M NaCl vs control"
     up2 <- dat$Regulation == "up"
     pcomp <- protcomp(dat$Entry, basis = basis, aa_file = file.path(extdatadir, "aa/bacteria/WJ02_aa.csv.xz"))
   } else if(study=="KSA+02") {
     # 20200419 Synechocystis, Kanesaki et al., 2002
     # KSA+02_NaCl, KSA+02_sorbitol
     dat <- read.csv(paste0(datadir, "KSA+02.csv.xz"), as.is=TRUE)
-    description <- paste("Synechocystis sp. PCC 6803 in 0.5 M", stage, "vs control")
+    description <- paste("_Synechocystis_ sp. PCC 6803 in 0.5 M", stage, "vs control")
     # use highly differential proteins for the indicated experiment
     icol <- grep(stage, colnames(dat))
     dat <- dat[abs(dat[, icol]) >= 3, ]
@@ -78,7 +78,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200419 E. coli, Gunasekera et al., 2008
     # GCP08_30, GCP08_43
     dat <- read.csv(paste0(datadir, "GCP08.csv.xz"), as.is=TRUE)
-    description <- paste("E. coli in 0.5 M vs 0 M NaCl at", stage, "deg C")
+    description <- paste("_Escherichia coli_ in 0.5 M vs 0 M NaCl at", stage, "deg C")
     if(stage == "30") icol <- grep("C2.C1", colnames(dat))
     if(stage == "43") icol <- grep("C5.C4", colnames(dat))
     dat <- dat[abs(dat[, icol]) > 1, ]
@@ -88,7 +88,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
   } else if(study=="LGW+05") {
     # 20200419 Shewanella oneidensis, Liu et al., 2005
     dat <- read.csv(paste0(datadir, "LGW+05.csv.xz"), as.is=TRUE)
-    description <- "Shewanella oneidensis MR-1 in 0.5 vs 0.1 M NaCl"
+    description <- "_Shewanella oneidensis_ MR-1 in 0.5 vs 0.1 M NaCl"
     up2 <- dat$AVG > 1
     dat <- cleanup(dat, "Entry", up2)
     pcomp <- protcomp(dat$Entry, basis = basis, aa_file = file.path(extdatadir, "aa/bacteria/LGW+05_aa.csv.xz"))
@@ -96,7 +96,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200419 Methylocystis, Han et al., 2017
     # HLL17_45min, HLL17_14h
     dat <- read.csv(paste0(datadir, "HLL17.csv.xz"), as.is=TRUE)
-    description <- paste("Methylocystis sp. strain SC2 in 0.75 % NaCl vs control at", stage)
+    description <- paste("_Methylocystis_ sp. strain SC2 in 0.75 % NaCl vs control at", stage)
     icol <- grep(stage, colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     up2 <- dat[, icol] > 0
@@ -105,7 +105,9 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200419 Listeria monocytogenes, Bergholz et al., 2012
     # BBWB12_37_2.5, 37_5, 37_10, 37_20, 37_exp, 7_2.5, 7_5, 7_10, 7_20, 7_exp
     dat <- read.csv(paste0(datadir, "BBWB12.csv.xz"), as.is=TRUE)
-    description <- paste("L. monocytogenes strain H7858 in 6 % NaCl vs control at", stage)
+    temp <- strsplit(stage, "_")[[1]][1]
+    phase <- strsplit(stage, "_")[[1]][2]
+    description <- paste("_Listeria monocytogenes_ strain H7858 in 6 % NaCl at", phase, "% lag-phase duration")
     icol <- grep(paste0("X", stage), colnames(dat))
     dat <- dat[abs(dat[, icol]) > 1, ]
     up2 <- dat[, icol] > 1
@@ -115,8 +117,8 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200419 Bacillus subtilis, Kohlstedt et al., 2014
     # KSM+14_NaCl, KSM+14_GB
     dat <- read.csv(paste0(datadir, "KSM+14.csv.xz"), as.is=TRUE)
-    if(stage=="NaCl") description <- "Bacillus subtilis in 1.2 M NaCl vs control"
-    if(stage=="GB") description <- "Bacillus subtilis with vs without glycine betaine in 1.2 M NaCl"
+    if(stage=="NaCl") description <- "_Bacillus subtilis_ in 1.2 M NaCl vs control"
+    if(stage=="GB") description <- "_Bacillus subtilis_ with vs without glycine betaine in 1.2 M NaCl"
     icol <- grep(stage, colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     up2 <- dat[, icol] > 0
@@ -126,7 +128,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200419 Enterococcus faecalis, Solheim et al., 2014
     # SLM+14_5, SLM+14_30, SLM+14_60
     dat <- read.csv(paste0(datadir, "SLM+14.csv.xz"), as.is=TRUE)
-    description <- paste("Enterococcus faecalis in 6.5 % NaCl vs control at", stage, "min")
+    description <- paste("_Enterococcus faecalis_ in 6.5 % NaCl vs control at", stage, "min")
     icol <- grep(stage, colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     up2 <- dat[, icol] > 0
@@ -135,7 +137,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
   } else if(study=="MWZ+18") {
     # 20200419 Lactobacillus paracasei, Ma et al., 2018
     dat <- read.csv(paste0(datadir, "MWZ+18.csv.xz"), as.is=TRUE)
-    description <- "Lactobacillus paracasei L9 with vs without 0.13% ox bile"
+    description <- "_Lactobacillus paracasei_ L9 with vs without 0.13% ox bile"
     up2 <- dat$Log2Fold_change > 0
     dat <- cleanup(dat, "Entry", up2)
     pcomp <- protcomp(dat$Entry, basis = basis, aa_file = file.path(extdatadir, "aa/bacteria/MWZ+18_aa.csv.xz"))
@@ -143,8 +145,8 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200419 Yersinia pestis, Han et al., 2005
     # HZP+05_HOS, HZP+05_HSS
     dat <- read.csv(paste0(datadir, "HZP+05.csv.xz"), as.is=TRUE)
-    if(stage=="HOS") description <- paste("Yersinia pestis in 0.5 M sorbitol vs control")
-    if(stage=="HSS") description <- paste("Yersinia pestis in 0.5 M NaCl vs control")
+    if(stage=="HOS") description <- paste("_Yersinia pestis_ in 0.5 M sorbitol vs control")
+    if(stage=="HSS") description <- paste("_Yersinia pestis_ in 0.5 M NaCl vs control")
     icol <- grep(stage, colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     dat <- dat[dat[, icol] >= 2 | dat[, icol] <= 0.5, ]
@@ -154,15 +156,15 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
   } else if(study=="SLA+05") {
     # 20200420 Synechocystis, Shapiguzov et al., 2005
     dat <- read.csv(paste0(datadir, "SLA+05.csv.xz"), as.is=TRUE)
-    description <- "Synechocystis sp. PCC 6803 in 0.5 M sorbitol vs control"
+    description <- "_Synechocystis_ sp. PCC 6803 in 0.5 M sorbitol vs control"
     up2 <- dat$IF > 0
     pcomp <- protcomp(dat$Entry, basis = basis, aa_file = file.path(extdatadir, "aa/bacteria/SLA+05_aa.csv.xz"))
   } else if(study=="LB12") {
     # 20200420 Synechococcus, Ludwig and Bryant, 2012
     # LB12_NaCl, LB12_glycerol
     dat <- read.csv(paste0(datadir, "LB12.csv.xz"), as.is=TRUE)
-    if(stage=="NaCl") description <- "Synechococcus sp. strain PCC 7002 in 1.5 M NaCl vs control"
-    if(stage=="glycerol") description <- "Synechococcus sp. strain PCC 7002 in 10 mM glycerol vs control"
+    if(stage=="NaCl") description <- "_Synechococcus_ sp. strain PCC 7002 in 1.5 M NaCl vs control"
+    if(stage=="glycerol") description <- "_Synechococcus_ sp. strain PCC 7002 in 10 mM glycerol vs control"
     icol <- grep(stage, colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     dat <- dat[dat[, icol] > 2 | dat[, icol] < 0.5, ]
@@ -175,7 +177,9 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # FRH+15_KCl_1h, FRH+15_KCl_6h, FRH+15_KCl_24h
     # FRH+15_glycerol_1h, FRH+15_glycerol_6h, FRH+15_glycerol_24h
     dat <- read.csv(paste0(datadir, "FRH+15.csv.xz"), as.is=TRUE)
-    description <- paste("Salmonella enterica", stage)
+    solute <- strsplit(stage, "_")[[1]][2]
+    time <- strsplit(stage, "_")[[1]][3]
+    description <- paste("_Salmonella enterica_ in", solute, "vs control for", time, "h")
     icol <- grep(stage, colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     up2 <- dat[, icol] > 1
@@ -185,7 +189,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200423 E. coli, Metris et al., 2014
     # MGM+14_3.5, MGM+14_4.5, MGM+14_5, MGM+14_5.5 
     dat <- read.csv(file.path(datadir, "MGM+14.csv.xz"), as.is=TRUE)
-    description <- paste("E. coli in", stage, "vs 2 % NaCl (with glycine betaine)")
+    description <- paste("_Escherichia coli_ in", stage, "vs 2 % NaCl (with glycine betaine)")
     icol <- grep(paste0("_", stage, "_"), colnames(dat))
     dat <- dat[abs(dat[, icol]) > 1, ]
     up2 <- dat[, icol] > 1
@@ -195,7 +199,7 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200423 E. coli, Shabala et al., 2009
     # SBB+09_NaCl, SBB+09_Sucrose
     dat <- read.csv(file.path(datadir, "SBB+09.csv.xz"), as.is=TRUE)
-    description <- paste("Escherichia coli in ~2.7 Os/kg", gsub("Sucrose", "sucrose", stage), "vs control")
+    description <- paste("_Escherichia coli_ in ~2.7 Os/kg", gsub("Sucrose", "sucrose", stage), "vs control")
     icol <- grep(stage, colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     up2 <- dat[, icol] == "up"
@@ -205,8 +209,8 @@ pdat_saltygenes <- function(dataset=2020, basis="rQEC") {
     # 20200423 E. coli in NaCl or urea, Withman et al., 2013
     # WGB+13_N, WGB+13_U
     dat <- read.csv(file.path(datadir, "WGB+13.csv.xz"), as.is=TRUE)
-    if(stage=="N") description <- "Escherichia coli in 0.3 M NaCl vs control"
-    if(stage=="U") description <- "Escherichia coli in 0.6 M urea vs control"
+    if(stage=="N") description <- "_Escherichia coli_ in 0.3 M NaCl vs control"
+    if(stage=="U") description <- "_Escherichia coli_ in 0.6 M urea vs control"
     icol <- grep(paste0(stage, ".vs.K"), colnames(dat))
     dat <- dat[!is.na(dat[, icol]), ]
     up2 <- dat[, icol] > 0
