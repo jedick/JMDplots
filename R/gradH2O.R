53c79400018b20d5c37fcdda67bfae16103fc6d7
jedick <j3ffdick@gmail.com>
2020-08-21 14:13:37 +0700

    Relabel gradH2O2() and remove gradH2O5()

--- R/gradH2O.R
+++ R/gradH2O.R
@@ -221,14 +221,28 @@ gradH2O1 <- function(pdf = FALSE) {
 # nH2O-ZC scatterplots for redox gradients and the Baltic Sea 20190713
 gradH2O2 <- function(pdf = FALSE, vars = "H2O-ZC", lm = NULL) {
   if(pdf) pdf("gradH2O2.pdf", width = 12, height = 5.6)
-  par(mfrow = c(1, 2))
-  par(mar = c(4, 4.5, 2, 1), las = 1, cex = 1.2)
+  layout(matrix(c(1, 2, 3), nrow = 1), widths = c(3, 2, 2))
+  par(mar = c(3.5, 4.5, 2, 1), las = 1, cex = 1.2)
+  par(mgp = c(2.5, 1, 0))
   par(cex.lab = 1.3)
 
+  # define axis labels
+  nH2Olab <- expression(italic(n)[H[2] * O])
+  ZClab <- expression(italic(Z)[C])
+
   # plot 1: compare ZC and nH2O of proteins in datasets from gradox paper
+  plot(c(-0.22, -0.12), c(-1.10, -1.02), xlab = ZClab, ylab = NA, type = "n")
+  mtext(nH2Olab, side = 2, las = 0, line = 3.2, cex = 1.6)
+  # add H2O-ZC guidelines 20200819
+  if(!is.null(lm)) {
+    x <- par("usr")[1:2]
+    y <- predict(lm, data.frame(ZC.aa = x))
+    for(dy in seq(-0.50, -0.60, -0.01)) lines(x, y + dy, col = "gray80")
+    box()
+  }
   mgradox <- ppage("gradoxGS", plot.it = FALSE)
   pgradox <- ppage("gradoxGS", H2O = TRUE, plot.it = FALSE)
-  pcomp(mgradox, pgradox, reorder = FALSE, yline = 3.2, vars = vars, cex.ylab = 1.5, font = 2, labdy = 0.003)
+  pcomp(mgradox, pgradox, reorder = FALSE, add = TRUE, vars = vars, font = 2, labdy = 0.003)
   # add proteomes from Nif-encoding genomes 20191014
   np <- NifProteomes()
   if(vars == "H2O-ZC") {
@@ -241,61 +255,72 @@ gradH2O2 <- function(pdf = FALSE, vars = "H2O-ZC", lm = NULL) {
   }
   # add text labels
   if(vars == "H2O-ZC") {
-    text(-0.21, -0.007, "hot spring", cex = 0.8)
-    text(-0.21, -0.0095, "source", cex = 0.8)
-    text(-0.157, -0.006, "photo-", adj = 0, cex = 0.8)
-    text(-0.157, -0.0085, "trophic", adj = 0, cex = 0.8)
-    text(-0.157, -0.011, "zone", adj = 0, cex = 0.8)
-    text(-0.17, -0.015, "photosynthetic", cex = 0.8)
-    text(-0.17, -0.018, "fringe", cex = 0.8)
+    text(-0.213, -1.0885, "hot spring", cex = 0.8, srt = 12)
+    text(-0.213, -1.091, "source", cex = 0.8, srt = 12)
+    text(-0.172, -1.062, "phototrophic", cex = 0.8, srt = 12)
+    text(-0.172, -1.0645, "zone", cex = 0.8, srt = 12)
     text(-0.127, -0.002, "> 3 mm", adj = 0, cex = 0.8)
-    text(-0.138, 0.001, "3 mm", adj = 0, cex = 0.8)
-    text(-0.134, 0.0035, "2 mm", adj = 0, cex = 0.8)
-    text(-0.131, 0.012, "1 mm", adj = 0, cex = 0.8)
-    text(-0.205, 0.011, "vent", cex = 0.8)
-    text(-0.205, 0.0085, "fluids", cex = 0.8)
-    text(-0.157, 0.013, "plume", cex = 0.8)
-    text(-0.1615, 0.005, "sea-", adj = 0, cex = 0.8)
-    text(-0.1615, 0.0025, "water", adj = 0, cex = 0.8)
-    text(c(-0.188, -0.212, -0.186, -0.161), c(0.0455, 0.0344, 0.0257, 0.0248), c("Nif-D", "Nif-C", "Nif-B", "Nif-A"), adj = 0, cex = 0.8)
+    text(-0.1395, -1.078, "3 mm", adj = 0, cex = 0.8)
+    text(-0.135, -1.0665, "2 mm", adj = 0, cex = 0.8)
+    text(-0.132, -1.0555, "1 mm", adj = 0, cex = 0.8)
+    text(-0.182, -1.094, "vent", cex = 0.8, srt = 12)
+    text(-0.182, -1.097, "  fluids", cex = 0.8, srt = 12)
+    text(-0.151, -1.079, "plume", cex = 0.8, srt = 12)
+    text(-0.151, -1.0815, "and", cex = 0.8, srt = 12)
+    text(-0.151, -1.0840, "  seawater", cex = 0.8, srt = 12)
+    text(c(-0.196, -0.222, -0.191, -0.161), c(-1.046, -1.0645, -1.055, -1.032), c("Nif-D", "Nif-C", "Nif-B", "Nif-A"), adj = 0, cex = 0.8)
     text(-0.218, 0.0365, "NF", cex=0.7, font = 2)
-    # add H2O-ZC fit for E. coli 20200819
-    if(!is.null(lm)) {
-      x <- par("usr")[1:2]
-      y <- predict(lm, data.frame(ZC.aa = x))
-      for(dy in seq(-0.50, -0.60, -0.01)) lines(x, y + dy, col = "gray80")
-    }
   }
   if(vars == "pIG") {
     text(c(5.00, 7.49, 5.65, 5.68), c(-0.123, -0.130, -0.109, -0.066), c("Nif-D", "Nif-C", "Nif-B", "Nif-A"), adj = 0)
     text(c(5.37, 5.42, 5.57), c(-0.158, -0.176, -0.216), c("1 mm", "2 mm", "3 mm"), adj = 1)
   }
   title("Redox gradients", font.main = 1)
-  label.figure("(a)", cex = 2, xfrac = 0.035)
+  label.figure("(a)", cex = 1.7, xfrac = 0.04)
 
   # plot 2: compare ZC and nH2O of proteins in Baltic Sea surface
+  plot(c(-0.18, -0.12), c(-1.10, -1.02), xlab = ZClab, ylab = NA, type = "n")
+  mtext(nH2Olab, side = 2, las = 0, line = 3.2, cex = 1.6)
+  # add H2O-ZC guidelines 20200819
+  if(!is.null(lm)) {
+    x <- par("usr")[1:2]
+    y <- predict(lm, data.frame(ZC.aa = x))
+    for(dy in seq(-0.50, -0.60, -0.01)) lines(x, y + dy, col = "gray80")
+    box()
+  }
   mbaltics <- ppage("balticsurface", plot.it = FALSE)
   pbaltics <- ppage("balticsurface", H2O = TRUE, plot.it = FALSE)
-  pcomp(mbaltics, pbaltics, reorder = FALSE, yline = 3.2, labels.at = NA, vars = vars, pch = list(c(2, 2, 2, 2, 17, 17, 17, 17, 17, 17)), cex.ylab = 1.8)
-  # compare ZC and nH2O of proteins in Baltic Sea 10-20 m
-  mbalticd <- ppage("balticdeep", plot.it = FALSE)
-  pbalticd <- ppage("balticdeep", H2O = TRUE, plot.it = FALSE)
-  pcomp(mbalticd, pbalticd, reorder = FALSE, add = TRUE, lty = 3, labels.at = NA, vars = vars, pch = list(c(6, 6, 6, 6, 25, 25, 25, 25, 25)))
+  pcomp(mbaltics, pbaltics, reorder = FALSE, add = TRUE, labels.at = NA, vars = vars, pch = list(c(17, 17, 17, 17, 19, 19, 19, 19, 19, 19)))
   # add text labels
   if(vars == "H2O-ZC") {
-    text(-0.14, 0.037, "surface\n< 6 PSU")
-    text(-0.135, 0.026, "surface\n> 6 PSU")
-    text(-0.185, 0.031, "chl a max\n< 6 PSU")
-    text(-0.18, 0.015, "chl a max\n> 6 PSU")
-    # add H2O-ZC fit for E. coli 20200819
-    if(!is.null(lm)) {
-      x <- par("usr")[1:2]
-      y <- predict(lm, data.frame(ZC.aa = x))
-      for(dy in seq(-0.50, -0.60, -0.01)) lines(x, y + dy, col = "gray80")
-    }
+    text(-0.138, -1.031, "< 6 PSU")
+    text(-0.139, -1.060, "> 6 PSU")
   }
-  title("Baltic Sea", font.main = 1)
-  label.figure("(b)", cex = 2, xfrac = 0.035)
+  title("Baltic Sea", font.main = 1, line = 1.2)
+  title("surface", font.main = 1, line = 0.2)
+  label.figure("(b)", cex = 1.7, xfrac = 0.035)
+
+  # plot 3: compare ZC and nH2O of proteins in Baltic Sea 10-20 m
+  plot(c(-0.18, -0.12), c(-1.10, -1.02), xlab = ZClab, ylab = NA, type = "n")
+  mtext(nH2Olab, side = 2, las = 0, line = 3.2, cex = 1.6)
+  # add H2O-ZC guidelines 20200819
+  if(!is.null(lm)) {
+    x <- par("usr")[1:2]
+    y <- predict(lm, data.frame(ZC.aa = x))
+    for(dy in seq(-0.50, -0.60, -0.01)) lines(x, y + dy, col = "gray80")
+    box()
+  }
+  mbalticd <- ppage("balticdeep", plot.it = FALSE)
+  pbalticd <- ppage("balticdeep", H2O = TRUE, plot.it = FALSE)
+  pcomp(mbalticd, pbalticd, reorder = FALSE, add = TRUE, labels.at = NA, vars = vars, pch = list(c(17, 17, 17, 17, 19, 19, 19, 19, 19)))
+  # add text labels
+  if(vars == "H2O-ZC") {
+    text(-0.151, -1.052, "< 6 PSU")
+    text(-0.136, -1.080, "> 6 PSU")
+  }
+  title("Baltic Sea", font.main = 1, line = 1.2)
+  title("chl a max", font.main = 1, line = 0.2)
+  label.figure("(c)", cex = 1.7, xfrac = 0.035)
 
   if(pdf) {
     dev.off()
@@ -429,56 +454,9 @@ gradH2O4 <- function(pdf = FALSE) {
   }
 }
 
-# scatterplots of ZC and nH2O vs GRAVY 20191117
-gradH2O5 <- function(pdf = FALSE) {
-  if(pdf) pdf("gradH2O5.pdf", width = 7, height = 33)
-  par(las = 1, mar = c(4, 4.2, 1, 1), mgp = c(2.5, 1, 0))
-  par(cex.lab = 1.3)
-  scatterfun <- function(xvar, yvar, AAcomp) {
-    if(yvar=="ZC") {
-      y <- ZCAA(AAcomp)
-      ylab <- expression(italic(Z)[C])
-    }
-    if(yvar=="nH2O") {
-      y <- H2OAA(AAcomp)
-      ylab <- expression(italic(n)[H[2] * O])
-    }
-    if(xvar=="GRAVY") {
-      x <- GRAVY(AAcomp)
-      xlab <- "GRAVY"
-    }
-    if(xvar=="pI") {
-      x <- pI(AAcomp)
-      xlab <- "pI"
-    }
-    # make scatterplot
-    smoothScatter(x, y, xlab = xlab, ylab = ylab, colramp = colorRampPalette(c("transparent", blues9)))
-    # add linear fit
-    mylm <- lm(y ~ x)
-    xs <- par("usr")[1:2]
-    ys <- predict(mylm, data.frame(x = xs))
-    lines(xs, ys, lty = 2, lwd = 2, col = "grey40")
-    # add legend with R-squared value
-    R2 <- format(round(summary(mylm)$r.squared, 2), nsmall = 2)
-    R2txt <- substitute(italic(R)^2 == R2, list(R2 = R2))
-    legend("bottomleft", legend = R2txt, bty = "n")
-  }
-  par(mfrow = c(1, 2))
-  # get amino acid compositions of E.coli proteins (UniProt)
-  ecoli <- read.csv(system.file("/extdata/organisms/ecoli.csv.xz", package = "JMDplots"), as.is = TRUE)
-  scatterfun("GRAVY", "ZC", ecoli)
-  label.figure("(a)", cex = 1.8)
-  scatterfun("GRAVY", "nH2O", ecoli)
-  label.figure("(b)", cex = 1.8)
-  if(pdf) {
-    dev.off()
-    addexif("gradH2O5", "Scatterplots of ZC and nH2O vs GRAVY", "Dick et al. (2020) (preprint)")
-  }
-}
-
 # nH2O-ZC and GRAVY-pI plots for Baltic Sea and Rodriguez-Brito et al. data 20200421
-gradH2O6 <- function(pdf = FALSE) {
-  if(pdf) pdf("gradH2O6.pdf", width = 8, height = 8)
+gradH2O5 <- function(pdf = FALSE) {
+  if(pdf) pdf("gradH2O5.pdf", width = 8, height = 8)
   layout(matrix(1:4, nrow = 2))
   par(mar = c(4, 4.5, 2, 1), las = 1, cex = 1.2)
   par(cex.lab = 1.3)
@@ -510,13 +488,13 @@ gradH2O6 <- function(pdf = FALSE) {
   label.figure("(d)", cex = 1.7)
   if(pdf) {
     dev.off()
-    addexif("gradH2O6", "nH2O-ZC and GRAVY-pI plots for Baltic Sea and Rodriguez-Brito et al. data", "Dick et al. (2020) (preprint)")
+    addexif("gradH2O5", "nH2O-ZC and GRAVY-pI plots for Baltic Sea and Rodriguez-Brito et al. data", "Dick et al. (2020) (preprint)")
   }
 }
 
 # differential gene and protein expression, time-course and NaCl vs organic solutes 20200420
-gradH2O7 <- function(pdf = FALSE) {
-  if(pdf) pdf("gradH2O7.pdf", width = 8, height = 4.5)
+gradH2O6 <- function(pdf = FALSE) {
+  if(pdf) pdf("gradH2O6.pdf", width = 8, height = 4.5)
   layout(matrix(0:11, nrow = 3, byrow = TRUE), widths = c(0.2, 1, 1, 1), heights = c(0.2, 1, 1))
   # add titles
   par(mar = c(0, 0, 0, 0))
@@ -652,7 +630,7 @@ gradH2O7 <- function(pdf = FALSE) {
 
   if(pdf) {
     dev.off()
-    addexif("gradH2O7", "differential gene and protein expression, time-course and NaCl vs organic solutes", "Dick et al. (2020) (preprint)")
+    addexif("gradH2O6", "differential gene and protein expression, time-course and NaCl vs organic solutes", "Dick et al. (2020) (preprint)")
   }
 }
 
@@ -661,8 +639,8 @@ gradH2O7 <- function(pdf = FALSE) {
 # add GRAVY and pI plot 20191028
 # use different symbols for eukaryotes and add halophilic bacteria and archaea 20191102-20191103
 # separate plots for halophiles and non-halophiles (no eukaryotes) 20200424
-gradH2O8 <- function(pdf = FALSE) {
-  if(pdf) pdf("gradH2O8.pdf", width = 5, height = 4)
+gradH2O7 <- function(pdf = FALSE) {
+  if(pdf) pdf("gradH2O7.pdf", width = 5, height = 4)
   layout(matrix(c(1, 1, 2, 2, 0, 3, 3, 0), nrow = 2, byrow = 2))
   par(mar = c(3.2, 3.2, 2, 1))
   par(mgp = c(1.9, 0.6, 0))
@@ -702,7 +680,7 @@ gradH2O8 <- function(pdf = FALSE) {
 
   if(pdf) {
     dev.off()
-    addexif("gradH2O8", "Differentially expressed proteins in halophiles under hypo- and hyperosmotic stress", "Dick et al. (2020) (preprint)")
+    addexif("gradH2O7", "Differentially expressed proteins in halophiles under hypo- and hyperosmotic stress", "Dick et al. (2020) (preprint)")
   }
 }
 
