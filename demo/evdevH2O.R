46112ab824cbbed1ecb5424eb13f6c333519cf8f
jedick <j3ffdick@gmail.com>
2022-11-24 15:50:23 +0800

    Update demo/evdevH2O.R to reflect published versions of plots

--- demo/evdevH2O.R
+++ demo/evdevH2O.R
@@ -1,42 +1,63 @@
 # JMDplots/demo/evdevH2O.R
-# Graphical abstract for research profile on website 20210203
+# Graphical abstract for research profile on https://chnosz.net/jeff 20210203
 # Moved to JMDplots 20210715
+# Update for published versions of plots 20221124
+
+# Original publication:
+# A thermodynamic model for water activity and redox potential in evolution and development
+# https://doi.org/10.1007/s00239-022-10051-7
+
 require("JMDplots")
-# png("evdevH2O.png", width = 800, height = 600)
+
+# Change this to TRUE to save the PNG file
+do.png <- FALSE
+if(do.png) png("evdevH2O.png", width = 800, height = 600)
 
 mat <- matrix(c(1,1,0, 1,1,0, 1,1,0, 1,2,2, 1,2,2, 0,2,2, 0,2,2), ncol = 3, byrow = TRUE)
 layout(mat)
 par(mar = c(3, 3.1, 3, 1), mgp = c(2.2, 0.5, 0), bg = "transparent")
 par(cex = 1.5)
-logH2Olab <- quote(phantom(.)~~~~~~~~~~~bold(log)*bolditalic(a)[bold(H[2]*O)])
-logO2lab <- quote(bold(log)*bolditalic(f)[bold(O[2])]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~phantom(.))
 
-# Get mean amino acid compositions
-gpa <- JMDplots:::getphyloaa("TPPG17")
-PS <- gpa$aa$protein
-# Load PS model proteins 
-ipPS <- add.protein(gpa$aa)
+## Plot 1: Maximum activity calculation on logaH2O-logfO2 diagram
+logH2Olab <- quote(bold(log)*bolditalic(a)[bold(H[2]*O)])
+logO2lab <- quote(bold(log)*bolditalic(f)[bold(O[2])]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~phantom(.))
+
 # Set up system
 basis("QEC")
 O2 <- c(-72, -67)
 H2O <- c(-3, 2)
-
-# Plot B: 16 PS model proteins and n = 200 sample of human proteins
-set.seed(3)
-iind <- sample(1:nrow(gpa$pcomp$aa), 200)
-ipind <- add.protein(gpa$pcomp$aa[iind, ])
-a <- affinity(O2 = c(O2, 128), H2O = c(H2O, 128), iprotein = c(ipPS, ipind))
+# Get mean amino acid compositions for phylostrata
+gpa <- JMDplots:::getphyloaa("TPPG17")
+PS <- gpa$aa$protein
+# Load PS model proteins 
+ipPS <- add.protein(gpa$aa)
+# Use n = 200 sample of human proteins for backgroiund
+nbackground <- 200
+# Use same background proteins as in MaximAct()
+TPPG17 <- read.csv(system.file(paste0("extdata/evdevH2O/phylostrata/TPPG17.csv.xz"), package = "JMDplots"), as.is = TRUE)
+LMM16 <- read.csv(system.file(paste0("extdata/evdevH2O/phylostrata/LMM16.csv.xz"), package = "JMDplots"), as.is = TRUE)
+Entry <- na.omit(intersect(TPPG17$Entry, LMM16$UniProt))
+aaback <- protcomp(Entry)$aa
+seed <- 24
+set.seed(seed)
+iback <- sample(1:nrow(aaback), nbackground)
+ipback <- add.protein(aaback[iback, ])
+# Calculate affinities
+a <- affinity(O2 = c(O2, 128), H2O = c(H2O, 128), iprotein = c(ipPS, ipback))
 e <- equilibrate(a, as.residue = TRUE, loga.balance = 0)
 d <- diagram(e, plot.it = FALSE)
-par.orig <- JMDplots:::my.filled.contour(e$vals$O2, e$vals$H2O, d$predominant.values, xlab = logO2lab, ylab = NA,
+
+# Use modified filled.contour() to add to existing plot layout
+JMDplots:::my.filled.contour(e$vals$O2, e$vals$H2O, d$predominant.values, xlab = NA, ylab = NA,
   nlevels = 50, col = hcl.colors(75, "YlGnBu")[20:75], plot.key = FALSE,
-  # use plot.axes to label the contour plot (see ?filled.contour)
+  # Use plot.axes to label the contour plot (see ?filled.contour)
   plot.axes = {
-    mtext(logH2Olab, side = 2, line = 2, las = 0, cex = par("cex"))
+    mtext(logO2lab, side = 1, line = 2.5, cex = 2)
+    mtext(logH2Olab, side = 2, line = 1.5, las = 0, cex = 2)
     names <- sapply(strsplit(d$species$name, "\\|"), "[", 2)
     #diagram(e, add = TRUE, names = names, format.names = FALSE)
     # Use a higher resolution for making the lines
-    a <- affinity(O2 = c(O2, 400), H2O = c(H2O, 400), iprotein = c(ipPS, ipind))
+    a <- affinity(O2 = c(O2, 400), H2O = c(H2O, 400), iprotein = c(ipPS, ipback))
     diagram(a, as.residue = TRUE, add = TRUE, names = FALSE, format.names = FALSE)
     opar <- par(tcl = 0.3)
     thermo.axis()
@@ -66,7 +87,7 @@ par.orig <- JMDplots:::my.filled.contour(e$vals$O2, e$vals$H2O, d$predominant.va
 ## Plot 2: Virtual Eh for Liebeskind gene ages
 PS_source <- "LMM16"
 # Read results
-datadir <- system.file("extdata/evdevH2O", package = "JMDplots")
+datadir <- system.file("extdata/evdevH2O/MaximAct", package = "JMDplots")
 H2Ofile <- file.path(datadir, paste0(PS_source, "_H2O_Hsa.csv"))
 O2file <- file.path(datadir, paste0(PS_source, "_O2_Hsa.csv"))
 H2O <- read.csv(H2Ofile, as.is = TRUE, check.names = FALSE)
@@ -108,22 +129,23 @@ labels <- c("Cellular\norganisms", "Euk_Archaea", "Euk+Bac", "Eukaryota", "Opist
 text(x = 1:8, y = par()$usr[3] - 1.5 * strheight("A"), labels = labels, srt = 45, adj = 1, xpd = TRUE)
 axis(1, at = 1:8, labels = NA)
 axis(2, at = seq(-300, -150, 50))
-# PS 1 (Cellular_organisms), 4 (Eukaryota), 6 (Eumetazoa), 8 (Mammalia)
-abline(v = c(1.02, 4, 6, 7.98), col = 5, lwd = 3)
-xtext <- 5.5
-# Eh = -150 mV (plasma GSH/GSSG) Jones and Sies, 2015
-# Eh = -199 mV (erythrocyte GSH/GSSG) van 't Erve et al., 2013
-# Eh = -241 mV (cytosolic NADH/NAD+) Jones and Sies, 2015
-# Eh = -318 mV (mitochondrial NADH/NAD+) Jones and Sies, 2015
-abline(h = c(-150, -199, -241, -318), lty = 2, lwd = 1.5, col = "slategray4")
+## PS 1 (Cellular_organisms), 4 (Eukaryota), 6 (Eumetazoa), 8 (Mammalia)
+#abline(v = c(1.02, 4, 6, 7.98), col = 5, lwd = 3)
+# Add lines for measured Eh
+abline(h = c(-150, -208, -230, -320, -380), lty = 3, lwd = 1.5, col = "slategray4")
+# Plasma GSH/GSSG from Jones and Sies (2015)
+xtext <- 5
+text(xtext, -150, "Blood Plasma", adj = c(0.5, 1.2))
+# ER, cytosol, and mitochondrial GSH/GSSG from Schwarzlander et al. (2016)
+text(xtext, -208, "ER", adj = c(0.5, 1.4))
+text(xtext, -230, "Cell Lysate", adj = c(0.5, 1.4))
+text(xtext, -320, "Cytosol", adj = c(0.5, -0.4))
+text(xtext, -380, "Mitochondria", adj = c(0.5, -0.3))
+# Plot virtual Eh
 lines(PS, mV, lwd = 2, col = 2) 
-text(2.85, -150, "Plasma GSH/GSSG", adj = c(0.5, 1.3))
-text(2.85, -199, "Erythrocyte GSH/GSSG", adj = c(0.5, -0.3))
-text(5.5, -241, "Cytosolic NADH/NAD+", adj = c(0.5, 1.3))
-text(5.5, -318, "Mitochondrial NADH/NAD+", adj = c(0.5, -0.3))
 
-# Run these to close and compress the PNG file
-if(FALSE) {
+if(do.png) {
+  # Close and compress the PNG file
   dev.off()
   system("pngquant evdevH2O.png")
   system("mv evdevH2O-fs8.png evdevH2O.png")
